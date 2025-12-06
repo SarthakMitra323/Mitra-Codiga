9cf0cff0691c6318ca839e9b6afc97e886a81f14
Sarthak Mitra <hello.connectsphere.official@gmail.com>
2025-12-06 21:22:15 +0530

    Add documentation route

--- web_app.py
+++ web_app.py
@@ -13,6 +13,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/docs')
+def docs():
+    return render_template('docs.html')
+
 @app.route('/api/execute', methods=['POST'])
 def execute_code():
     try:
